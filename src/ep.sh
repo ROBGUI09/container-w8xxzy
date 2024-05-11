78c0f8faa6de71df0183963c224a62ff3779179f
Роберт on Infinix X6716 <pobept46.2002@gmail.com>
2024-05-11 15:28:51 +0500

    fix publicport

--- src/ep.sh
+++ src/ep.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-/usr/sbin/doh-proxy -H 'container-w8xxzy.containers.anotherwebservice.com' -u 127.0.0.1:53 -g 0.0.0.0:443 &
+/usr/sbin/doh-proxy -H 'container-w8xxzy.containers.anotherwebservice.com' -u 127.0.0.1:53 -g 0.0.0.0 -j 443 &
 
 exec /usr/sbin/smartdns -f -x 2>&1
