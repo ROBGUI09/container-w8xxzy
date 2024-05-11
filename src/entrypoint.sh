2b9a1cb903ac7f5d84e082dcffedd2348ec2d0d9
Роберт on Infinix X6716 <pobept46.2002@gmail.com>
2024-05-11 12:33:16 +0500

    replace flask with smartdns server

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,4 +2,4 @@
 set -ex
 
 # Start gunicorn, listening on port 500, access log to stdout
-exec gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
+exec docker run -d --name smartdns --restart=always -p 5000:5000/tcp -v /etc/smartdns:/etc/smartdns pymumu/smartdns:latest
