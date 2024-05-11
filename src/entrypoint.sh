ac2a77ac6df2a353c1477e07ada12bc87f0c02a4
Роберт on Infinix X6716 <pobept46.2002@gmail.com>
2024-05-11 12:47:16 +0500

    i hate docker

--- src/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-
-# Start gunicorn, listening on port 500, access log to stdout
-exec docker run -d --name smartdns --restart=always -p 5000:5000/tcp -v /etc/smartdns:/etc/smartdns pymumu/smartdns:latest
