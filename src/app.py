341c83db2654a4b5c4b9e1bb87d611269d956abc
Роберт on Infinix X6716 <pobept46.2002@gmail.com>
2024-05-13 11:47:47 +0500

    fix requests

--- src/app.py
+++ src/app.py
@@ -19,13 +19,13 @@ def proxy():
     path = "/dns-query"
     SITE_NAME = "https://127.0.0.1:3000"
     if request.method=="GET":
-        resp = requests.get(f"{SITE_NAME}{path}",verify='/etc/smartdns/smartdns-cert.pem')
+        resp = requests.get(f"{SITE_NAME}{path}",verify='/etc/smartdns/smartdns-key.pem')
         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
         headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
         return response
     elif request.method=="POST":
-        resp = requests.post(f"{SITE_NAME}{path}",data=request.form,verify='/etc/smartdns/smartdns-cert.pem')
+        resp = requests.post(f"{SITE_NAME}{path}",data=request.form,verify='/etc/smartdns/smartdns-key.pem')
         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
--- src/ep.sh
+++ src/ep.sh
@@ -2,7 +2,10 @@
 
 set -ex
 
+openssl genrsa -out smartdns-key.pem 2048
+openssl req -x509 -sha256 -new -nodes -key smartdns-key.pem -days 3650 -out smartdns-cert.pem
+
 #gunicorn -w 4 -b '0.0.0.0:5000' --debug --access-logfile=- 'app:app' &
-python3 app.py
+python3 app.py &
 
 exec /usr/sbin/smartdns -f -x 2>&1
