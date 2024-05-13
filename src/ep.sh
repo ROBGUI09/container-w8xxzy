a38adfa6a8ddbb2d03507805da17e800ddc17806
Роберт on Infinix X6716 <pobept46.2002@gmail.com>
2024-05-13 11:59:21 +0500

    add openssl fix #2

--- src/ep.sh
+++ src/ep.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-openssl genrsa -out smartdns-key.pem 2048
-openssl req -x509 -sha256 -new -nodes -key smartdns-key.pem -days 3650 -out smartdns-cert.pem
-
 #gunicorn -w 4 -b '0.0.0.0:5000' --debug --access-logfile=- 'app:app' &
 python3 app.py &
 
